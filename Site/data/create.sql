0cfb6c87d4da1d36a7e5e14520ea55c8f8026a72
HELLO Adam <helloa@hostname.iut2.upmf-grenoble.fr>
2021-11-19 14:10:01 +0100

    premiere version du create.sql

--- Site/data/create.sql
+++ Site/data/create.sql
@@ -1,15 +1,87 @@
-CREATE TABLE article (
-	ref INTEGER PRIMARY KEY,
-	libelle TEXT,
-	categorie INTEGER,
-	prix REAL,
-	image TEXT,
-	FOREIGN KEY(categorie) REFERENCES categorie(id)
+CREATE TABLE CANDIDAT (
+	idCandidat INTEGER PRIMARY KEY,
+	nom TEXT,
+	prenom TEXT,
+	age INTEGER,
+	adressePostale TEXT,
+	telephone TEXT,
+	lienCV TEXT
 );
 
-CREATE TABLE categorie (
-	id INTEGER PRIMARY KEY,
+CREATE TABLE COACH (
+	idCoach INTEGER PRIMARY KEY,
+	adresseMail TEXT,
 	nom TEXT,
-	pere INTEGER,
-	FOREIGN KEY(pere) REFERENCES categorie(id)
+	prenom TEXT
 );
+
+CREATE TABLE CLIENT (
+	idClient INTEGER PRIMARY KEY,
+	nomClient TEXT,
+	adressePostale TEXT,
+	mailClient TEXT,
+	telephone TEXT
+);
+
+CREATE TABLE OFFRE (
+	idOffre INTEGER PRIMARY KEY,
+	nomOffre TEXT,
+	dateOffre DATE
+);
+
+CREATE TABLE LOGIN (
+	idLogin INTEGER PRIMARY KEY,
+	adresseMail TEXT,
+	password TEXT
+);
+
+CREATE TABLE Message (
+	idMessage INTEGER PRIMARY KEY,
+	message TEXT,
+	dateEnvoi TEXT
+);
+
+CREATE TABLE DetailOffre (
+	idDetailOffre INTEGER PRIMARY KEY,
+	travEtranger TEXT,
+	secteurOffre TEXT,
+	typeContratOffre TEXT,
+	posteOffre TEXT,
+	typpeEntreprise TEXT
+);
+
+CREATE TABLE CompetenceRequise (
+	idCompetanceRequise INTEGER PRIMARY KEY,
+	nvEtude TEXT,
+	langueRequise TEXT,
+	langageRequis TEXT
+);
+
+CREATE TABLE Preference (
+	idPreference INTEGER PRIMARY KEY,
+	secteurPref TEXT,
+	typeContratPref TEXT,
+	postePref TEXT,
+	tyeEntreprisePref TEXT
+);
+
+CREATE TABLE CompetenceAcquise (
+	idCompetenceAcquise INTEGER PRIMARY KEY,
+	nvEtude TEXT,
+	langueParlee TEXT,
+	langageAcquis TEXT
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
