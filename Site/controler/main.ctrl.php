44d0d46c6f18e8e5a5bd031dd374dd29287c21a0
SEGHIR Younous <seghiry@hostname.iut2.upmf-grenoble.fr>
2021-11-19 15:11:41 +0100

    message

--- Site/controler/main.ctrl.php
+++ Site/controler/main.ctrl.php
@@ -7,7 +7,7 @@
 include_once(__DIR__."/../framework/view.class.php");
 
 // Inclusion du modèle
-include_once(__DIR__."/../model/DAO.class.php");
+//include_once(__DIR__."/../model/DAO.class.php");
 
 
 ////////////////////////////////////////////////////////////////////////////
--- Site/view/vue1.view.php
+++ Site/view/vue1.view.php
@@ -8,16 +8,13 @@
   </head>
   <body>
 
-    <!-- Premier header du site -->
+    <!-------------------------------------- Header du site --------------------------------------->
     <header  class="header1">
       <h1>Tech A Way</h1>
-    </header>
 
-    <!-- Deuxieme header du site -->
-    <header class="header2">
       <img class="logo"  src="../view/design/logo.png">
 
-      <ul>
+      <ul class="barNav">
         <li> <a href="login.view.php">Redirection 1</a> </li>
         <li> <a href="login.view.php">Redirection 2</a> </li>
         <li> <a href="login.view.php">Redirection 3</a> </li>
@@ -27,20 +24,46 @@
       <button class="signupButton" type="button"><a href="login.view.php">Sign Up</a></button>
     </header>
 
-    <!-- Contenu acceuil avec illustration -->
-    <div class="contenuAcceuil1">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus augue sed purus venenatis, ut pharetra elit dignissim. Fusce vel ex ornare, vulputate dui non, blandit arcu. Aenean laoreet, nibh tempor aliquam hendrerit, dui mi efficitur diam, sit amet porttitor diam dolor ac odio. Donec non ullamcorper mauris. Quisque elementum tortor quis augue elementum iaculis. Vivamus sit amet ultricies ligula. Nam fringilla quam id orci aliquam imperdiet. Phasellus eget vehicula neque. Vestibulum hendrerit metus quis ex viverra, vitae pretium sem tincidunt. Phasellus in nisi sed dui efficitur eleifend vel ac orci. Nam commodo ipsum at facilisis tempus. Sed interdum est vitae odio tempus feugiat. Etiam viverra iaculis dictum. Suspendisse eget ex et neque scelerisque lobortis. Ut dignissim interdum orci.  </p>
-        <img class="illustrationAcceuil" src="../view/design/logo.png">
+    <!-------------------------------------- Main du Site -------------------------------------->
+    <main>
 
-    </div>
 
-    <!-- Paragraphe de contenu sur tech a way -->
-    <div class="contenuAcceuil2">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus augue sed purus venenatis, ut pharetra elit dignissim. Fusce vel ex ornare, vulputate dui non, blandit arcu. Aenean laoreet, nibh tempor aliquam hendrerit, dui mi efficitur diam, sit amet porttitor diam dolor ac odio. Donec non ullamcorper mauris. Quisque elementum tortor quis augue elementum iaculis. Vivamus sit amet ultricies ligula. Nam fringilla quam id orci aliquam imperdiet. Phasellus eget vehicula neque. Vestibulum hendrerit metus quis ex viverra, vitae pretium sem tincidunt. Phasellus in nisi sed dui efficitur eleifend vel ac orci. Nam commodo ipsum at facilisis tempus. Sed interdum est vitae odio tempus feugiat. Etiam viverra iaculis dictum. Suspendisse eget ex et neque scelerisque lobortis. Ut dignissim interdum orci.  </p>
-    </div>
+      <section>
+        <div >
+          <h1></h1>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus augue sed purus venenatis, ut pharetra elit dignissim. Fusce vel ex ornare, vulputate dui non, blandit arcu. Aenean laoreet, nibh tempor aliquam hendrerit, dui mi efficitur diam, sit amet porttitor diam dolor ac odio. Donec non ullamcorper mauris. Quisque elementum tortor quis augue elementum iaculis. Vivamus sit amet ultricies ligula. Nam fringilla quam id orci aliquam imperdiet. Phasellus eget vehicula neque. Vestibulum hendrerit metus quis ex viverra, vitae pretium sem tincidunt. Phasellus in nisi sed dui efficitur eleifend vel ac orci. Nam commodo ipsum at facilisis tempus. Sed interdum est vitae odio tempus feugiat. Etiam viverra iaculis dictum. Suspendisse eget ex et neque scelerisque lobortis. Ut dignissim interdum orci.  </p>
+        </div>
 
-  
+        <div>
+          <img class="illustrationAcceuil" src="../view/design/logo.png">
+        </div>
 
+      <section>
+        <div class="contenuAcceuil2">
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus augue sed purus venenatis, ut pharetra elit dignissim. Fusce vel ex ornare, vulputate dui non, blandit arcu. Aenean laoreet, nibh tempor aliquam hendrerit, dui mi efficitur diam, sit amet porttitor diam dolor ac odio. Donec non ullamcorper mauris. Quisque elementum tortor quis augue elementum iaculis. Vivamus sit amet ultricies ligula. Nam fringilla quam id orci aliquam imperdiet. Phasellus eget vehicula neque. Vestibulum hendrerit metus quis ex viverra, vitae pretium sem tincidunt. Phasellus in nisi sed dui efficitur eleifend vel ac orci. Nam commodo ipsum at facilisis tempus. Sed interdum est vitae odio tempus feugiat. Etiam viverra iaculis dictum. Suspendisse eget ex et neque scelerisque lobortis. Ut dignissim interdum orci.  </p>
+        </div>
+      </section>
+
+      <section>
+        <div class="collectionAcceuil">
+          <h1> Nos Partenaires </h1>
+          <div> <img class="ImagePartenaire"  src="../view/design/logo.png"> </div>
+          <div> <img class="ImagePartenaire"  src="../view/design/logo.png"> </div>
+          <div> <img class="ImagePartenaire"  src="../view/design/logo.png"></div>
+          </div>
+      <section>
+
+    </main>
+
+    <section>
+      <form>
+      </form>
+    </section>
+
+    <!---------------------------------------- Footer du site ---------------------------------------->
+    <footer>
+
+    </footer>
 
 
   </body>
