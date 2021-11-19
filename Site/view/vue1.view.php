dbee67f9c95feb2940e540f2ecfd36762eb67507
SEGHIR Younous <seghiry@hostname.iut2.upmf-grenoble.fr>
2021-11-19 14:24:02 +0100

    message

--- Site/view/vue1.view.php
+++ Site/view/vue1.view.php
@@ -3,9 +3,45 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Vue 1</title>
+    <link rel="stylesheet" type="text/css" href="../view/design/style.css">
+    <title>Tech A Way</title>
   </head>
   <body>
+
+    <!-- Premier header du site -->
+    <header  class="header1">
+      <h1>Tech A Way</h1>
+    </header>
+
+    <!-- Deuxieme header du site -->
+    <header class="header2">
+      <img class="logo"  src="../view/design/logo.png">
+
+      <ul>
+        <li> <a href="login.view.php">Redirection 1</a> </li>
+        <li> <a href="login.view.php">Redirection 2</a> </li>
+        <li> <a href="login.view.php">Redirection 3</a> </li>
+      </ul>
+
+      <button class="loginButton" type="button"><a href="login.view.php">Login</a></button>
+      <button class="signupButton" type="button"><a href="login.view.php">Sign Up</a></button>
+    </header>
+
+    <!-- Contenu acceuil avec illustration -->
+    <div class="contenuAcceuil1">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus augue sed purus venenatis, ut pharetra elit dignissim. Fusce vel ex ornare, vulputate dui non, blandit arcu. Aenean laoreet, nibh tempor aliquam hendrerit, dui mi efficitur diam, sit amet porttitor diam dolor ac odio. Donec non ullamcorper mauris. Quisque elementum tortor quis augue elementum iaculis. Vivamus sit amet ultricies ligula. Nam fringilla quam id orci aliquam imperdiet. Phasellus eget vehicula neque. Vestibulum hendrerit metus quis ex viverra, vitae pretium sem tincidunt. Phasellus in nisi sed dui efficitur eleifend vel ac orci. Nam commodo ipsum at facilisis tempus. Sed interdum est vitae odio tempus feugiat. Etiam viverra iaculis dictum. Suspendisse eget ex et neque scelerisque lobortis. Ut dignissim interdum orci.  </p>
+        <img class="illustrationAcceuil" src="../view/design/logo.png">
+
+    </div>
+
+    <!-- Paragraphe de contenu sur tech a way -->
+    <div class="contenuAcceuil2">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus augue sed purus venenatis, ut pharetra elit dignissim. Fusce vel ex ornare, vulputate dui non, blandit arcu. Aenean laoreet, nibh tempor aliquam hendrerit, dui mi efficitur diam, sit amet porttitor diam dolor ac odio. Donec non ullamcorper mauris. Quisque elementum tortor quis augue elementum iaculis. Vivamus sit amet ultricies ligula. Nam fringilla quam id orci aliquam imperdiet. Phasellus eget vehicula neque. Vestibulum hendrerit metus quis ex viverra, vitae pretium sem tincidunt. Phasellus in nisi sed dui efficitur eleifend vel ac orci. Nam commodo ipsum at facilisis tempus. Sed interdum est vitae odio tempus feugiat. Etiam viverra iaculis dictum. Suspendisse eget ex et neque scelerisque lobortis. Ut dignissim interdum orci.  </p>
+    </div>
+
+  
+
+
 
   </body>
 </html>
