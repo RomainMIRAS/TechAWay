b067758020d5fa61f511765c7556251a9726cb26
SEGHIR Younous <seghiry@hostname.iut2.upmf-grenoble.fr>
2021-11-19 15:25:40 +0100

    maquette acceuil

--- Site/view/vue1.view.php
+++ Site/view/vue1.view.php
@@ -9,43 +9,43 @@
   <body>
 
     <!-------------------------------------- Header du site --------------------------------------->
-    <header  class="header1">
+    <header>
       <h1>Tech A Way</h1>
 
-      <img class="logo"  src="../view/design/logo.png">
+      <img src="../view/design/logo.png">
 
-      <ul class="barNav">
+      <ul>
         <li> <a href="login.view.php">Redirection 1</a> </li>
         <li> <a href="login.view.php">Redirection 2</a> </li>
         <li> <a href="login.view.php">Redirection 3</a> </li>
       </ul>
 
-      <button class="loginButton" type="button"><a href="login.view.php">Login</a></button>
-      <button class="signupButton" type="button"><a href="login.view.php">Sign Up</a></button>
+      <button type="button"><a href="login.view.php">Login</a></button>
+      <button type="button"><a href="login.view.php">Sign Up</a></button>
     </header>
 
     <!-------------------------------------- Main du Site -------------------------------------->
     <main>
 
 
-      <section>
+      <section class="contenuAcceuil1">
         <div >
           <h1></h1>
           <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus augue sed purus venenatis, ut pharetra elit dignissim. Fusce vel ex ornare, vulputate dui non, blandit arcu. Aenean laoreet, nibh tempor aliquam hendrerit, dui mi efficitur diam, sit amet porttitor diam dolor ac odio. Donec non ullamcorper mauris. Quisque elementum tortor quis augue elementum iaculis. Vivamus sit amet ultricies ligula. Nam fringilla quam id orci aliquam imperdiet. Phasellus eget vehicula neque. Vestibulum hendrerit metus quis ex viverra, vitae pretium sem tincidunt. Phasellus in nisi sed dui efficitur eleifend vel ac orci. Nam commodo ipsum at facilisis tempus. Sed interdum est vitae odio tempus feugiat. Etiam viverra iaculis dictum. Suspendisse eget ex et neque scelerisque lobortis. Ut dignissim interdum orci.  </p>
         </div>
 
         <div>
-          <img class="illustrationAcceuil" src="../view/design/logo.png">
+          <img src="../view/design/logo.png">
         </div>
 
-      <section>
-        <div class="contenuAcceuil2">
+      <section class="contenuAcceuil2">
+        <div>
           <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus augue sed purus venenatis, ut pharetra elit dignissim. Fusce vel ex ornare, vulputate dui non, blandit arcu. Aenean laoreet, nibh tempor aliquam hendrerit, dui mi efficitur diam, sit amet porttitor diam dolor ac odio. Donec non ullamcorper mauris. Quisque elementum tortor quis augue elementum iaculis. Vivamus sit amet ultricies ligula. Nam fringilla quam id orci aliquam imperdiet. Phasellus eget vehicula neque. Vestibulum hendrerit metus quis ex viverra, vitae pretium sem tincidunt. Phasellus in nisi sed dui efficitur eleifend vel ac orci. Nam commodo ipsum at facilisis tempus. Sed interdum est vitae odio tempus feugiat. Etiam viverra iaculis dictum. Suspendisse eget ex et neque scelerisque lobortis. Ut dignissim interdum orci.  </p>
         </div>
       </section>
 
-      <section>
-        <div class="collectionAcceuil">
+      <section class="listePartenaires">
+        <div >
           <h1> Nos Partenaires </h1>
           <div> <img class="ImagePartenaire"  src="../view/design/logo.png"> </div>
           <div> <img class="ImagePartenaire"  src="../view/design/logo.png"> </div>
@@ -55,14 +55,18 @@
 
     </main>
 
-    <section>
+    <section class="contactAcceuil">
       <form>
       </form>
     </section>
 
     <!---------------------------------------- Footer du site ---------------------------------------->
     <footer>
-
+        <ul>
+          <li> <a href="login.view.php">Lien footer 1</a> </li>
+          <li> <a href="login.view.php">Lien footer 2</a> </li>
+          <li> <a href="login.view.php">Lien footer 3</a> </li>
+        </ul>
     </footer>
 
 
