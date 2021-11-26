1b91d87a786e02ab6a6e4d25dcbff5eb11e161ca
HELLO Adam <helloa@hostname.iut2.upmf-grenoble.fr>
2021-11-26 16:15:26 +0100

    merge

--- Site/data/insert.sql
+++ Site/data/insert.sql
@@ -1,14 +1,9 @@
 SET datestyle TO european;
 
 -- insertion dans CANDIDAT ((attribut))
-<<<<<<< HEAD
 -- insert into CANDIDAT values("attributs");
 insert into UTILIISATEUR values(1, 'adresse-candidat@gmail.com', 'motdepassecandidat', 'shi', 'jean', 23, '06 11 22 33 44', now());
 insert into COMPETENCE values(1, 'bac+3', 'français, anglais', 'java, c, c++, python');
 insert into RENSEIGNEMENT values(1, false, 'programmation', 'CDI', 'développeur', 'Start-up');
 
 insert into CANDIDAT(1, null, null, 1, 'france', 'grenoble', 1, 1);
-=======
-
-insert into CANDIDAT values("attributs", , , , now());
->>>>>>> 3caef16e0e6812f9f47d85bdca7297017f28f017
