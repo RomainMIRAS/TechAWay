3caef16e0e6812f9f47d85bdca7297017f28f017
Remi Del Medico <remi.del-medico@etu.univ-grenoble-alpes.fr>
2021-11-26 16:00:21 +0100

    ide git > atom

--- Site/controler/formulaire.ctrl.php
+++ Site/controler/formulaire.ctrl.php
@@ -10,42 +10,8 @@ $nom = '';
 if (isset($_GET['nom'])) {
   $nom = $_GET['nom'];
 }
+$pays = array('Allemagne','Autriche','Andorre','Belgique','Boznie_Herzegovine','Bulgarie','Chypre','Croatie','Danemark','Espagne','Estonie','Finlande','France','Gibraltar','Grece','Hongrie','Irlande','Islande','italie','Lettonie','Liechtenstein','Lituanie','Luxembourg','Malte','Monaco','Norvege','Pays Bas','Pays de Galle','Pologne','Portugal','Republique_Tcheque','Roumanie','Royaume_Uni','Russie','Slovaquie','Slovenie','Suede','Suisse','Ukraine','Vatican');
 
-$pays = array('Allemagne','Autriche','Andorre','Belgique','Boznie_Herzegovine','Bulgarie','Chypre',
-'Croatie',
-'Danemark',
-'Espagne',
-'Estonie',
-'Finlande',
-'France',
-'Gibraltar',
-'Grece',
-'Hongrie',
-'Irlande',
-'Islande',
-'italie',
-'Lettonie',
-'Liechtenstein',
-'Lituanie',
-'Luxembourg',
-'Malte',
-'Monaco',
-'Norvege',
-'Pays Bas',
-'Pays de Galle',
-'Pologne',
-'Portugal',
-'Republique_Tcheque',
-'Roumanie',
-'Royaume_Uni',
-'Russie',
-'Slovaquie',
-'Slovenie',
-'Suede',
-'Suisse',
-'Ukraine',
-'Vatican'
-);
 $action = (isset($_GET['action'])) ? $_GET['action']: 'formulaire';
 $etape = (isset($_GET['etape'])) ? $_GET['etape']: 'base';
 
