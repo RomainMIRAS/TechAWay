ed3a59283fe059fcdfbfaa1cf601d70438a2952e
Remi Del Medico <remi.del-medico@etu.univ-grenoble-alpes.fr>
2021-11-26 14:29:28 +0100

    Update Site/model/Discussion.class.php

---
+++ Site/model/Discussion.class.php
@@ -0,0 +1,38 @@
+
+
+<?php
+
+// Description d'une discussion  
+class Discussion {
+  private int $id;                     //id de la discussion
+  private string $dateDiscussion        //date de la Discussion
+  private Candidat $correspondant1      //Candidat de la discussion
+  private Coach $correspondant2         //Coach de la discussion
+
+  // Contructeur
+  function __construct(Candidat $correspodant1, Coach $correspodant2) {
+    $this->correspondant1 = $correspondant1;
+    $this->correspondant2 = $correspondant2;
+  }
+
+  function getId() : int {
+    return $this->id;
+  }
+
+  function getDateDiscussion() : string {
+    return $this->dateDiscussion;
+  }
+
+  function getCorrespondant1() : Candidat {
+    return $this->correspondant1;
+  }
+
+  function getCorrespondant2() : Coach {
+    return $this->correspondant2;
+  }
+
+}
+
+
+
+?>
