96abbfb5bc1227f0f41b2ff6cd5d06e18d3c2beb
MIRAS Romain <mirasr@hostname.iut2.upmf-grenoble.fr>
2021-11-27 20:02:01 +0100

    a

--- Site/controler/authentification.ctrl.php
+++ Site/controler/authentification.ctrl.php
@@ -20,6 +20,7 @@ $erreur = "";
 //Set le type d'action (SignUp ou Login). Par défaut login
 $action = (isset($_POST['action'])) ? $_POST['action']:"login";
 $arrayName = array('adressemail' => "adresse-candidat@gmail.com");
+$arrayName = array('' => , );
 //array_values($dao->getEmails()
 
 
@@ -33,7 +34,7 @@ if ($confirmation == "oui" && $action == "signup"){
     $erreur = "Mot de passe doit avoir au minimum 8 caractères";
   } else if($password != $checkpassword){ // Mot de passe identique
     $erreur = "Les mots de passes doivent être identiques !";
-  } else if (in_array($email,$arrayName))){ // Si email déja enregistrée
+  } else if (in_array($email,$arrayName)){ // Si email déja enregistrée
     $erreur = $email;
   } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){ // Si email valide
     $erreur = "Email non valide !";
