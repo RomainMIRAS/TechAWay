0b0bb23aa3967f16101d38571b0412c60fca5f9d
devkosov <devkosov@github.com>
2021-11-27 20:15:36 +0100

    pagman

--- Site/model/DAO.class.php
+++ Site/model/DAO.class.php
@@ -30,7 +30,7 @@ function getEmails() : array {
 
 	$table = pg_fetch_all($req);
 
-	var_dump($table[0]);
+	var_dump($table[0]['adresseMail']);
 	$listeMail = array();
 	foreach ($table as $mail) {
 		array_push($listeMail,$mail);
