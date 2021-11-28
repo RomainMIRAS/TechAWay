94172757ded92aa029f09ec4ad0eb3269647fe8a
MIRAS Romain <mirasr@hostname.iut2.upmf-grenoble.fr>
2021-11-28 09:57:55 +0100

    Retest

--- Site/view/authentification.view.php
+++ Site/view/authentification.view.php
@@ -16,11 +16,11 @@
     <!-- GOOGLE FONT -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
+
   </head>
   <body>
 
@@ -43,7 +43,7 @@
             <label for="password">Mot de passe</label>
             <input id="password" type="password" name="password" placeholder="Mot de passe" required>
             <button type="submit" name="confirmation" value="oui">Confirmation</button>
-            <input type="hidden" name="action" value="<?$action ?>">
+            <input type="hidden" name="action" value="<?= $action ?>">
           </form>
 
         <!-- Si l'utilisateur clique sur "S'inscrire" -->
@@ -59,7 +59,7 @@
             <label for="checkpassword">Confirmation du mot de passe</label>
             <input id="checkpassword" type="password" name="checkpassword" placeholder="Mot de passe" required>
             <button type="submit" name="confirmation" value="oui">Confirmation</button>
-            <input type="hidden" name="action" value="signup">
+            <input type="hidden" name="action" value="<?= $action ?>">
           </form>
 
         <?php endif; ?>
