0506b73c36014738a494347d52739db0ee0db90f
devkosov <devkosov@github.com>
2021-11-30 22:18:35 +0100

    testing

--- Site/model/DAO.class.php
+++ Site/model/DAO.class.php
@@ -99,6 +99,8 @@ function getCoach(string $mail) {
 
 			$coachUti = pg_fetch_all($req);
 
+			echo '<pre>' . var_export($coachUti, true) . '</pre>';
+
 			$coach = new Coach(
 				$coachbf[0]['adressemail'],
 				$coachbf[0]['password'],
