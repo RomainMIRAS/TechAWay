8b31d61a4836fd4b6447caf87272827a128a6993
devkosov <devkosov@github.com>
2021-11-30 22:19:54 +0100

    testing

--- Site/model/DAO.class.php
+++ Site/model/DAO.class.php
@@ -94,7 +94,7 @@ function getCoach(string $mail) {
 			return false;
 		}else{
 
-			$req = pg_query($this->db,"SELECT * FROM coach WHERE idcoach=". intVal($coachbf[0]['idUtilisateur']) ."");
+			$req = pg_query($this->db,"SELECT * FROM coach WHERE idcoach=". intVal($coachbf[0]['idutilisateur']) ."");
 
 
 			$coachUti = pg_fetch_all($req);
