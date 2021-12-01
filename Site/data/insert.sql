b437709d4a93babeca647a25653d26ccdb7d152f
devkosov <devkosov@github.com>
2021-12-01 19:56:23 +0100

    dao

--- Site/data/insert.sql
+++ Site/data/insert.sql
@@ -6,7 +6,7 @@ insert into utilisateur values(DEFAULT,'adresse-candidat@gmail.com', 'motdepasse
 insert into competence values(DEFAULT,'bac+3', 'français, anglais', 'java, c, c++, python');
 insert into renseignement values(DEFAULT,false, 'programmation', 'CDI', 'développeur', 'Start-up');
 
-insert into candidat values(377, NULL, NULL, 1, 'france', 'grenoble', 1, 1);
+insert into candidat values(428, NULL, NULL, 1, 'france', 'grenoble', 1, 1);
 
 
 insert into utilisateur values(DEFAULT,'Candidat2@gmail.com', 'candidat2', 'Jean', 'Paul', 23, '06 11 22 55 44', now());
