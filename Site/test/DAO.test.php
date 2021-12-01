0ac09ab9ea4553cb76b3b31c8f5ec6ed800ff7a4
devkosov <devkosov@github.com>
2021-12-01 19:07:47 +0100

    dao

--- Site/test/DAO.test.php
+++ Site/test/DAO.test.php
@@ -3,7 +3,7 @@ require_once(__DIR__.'/../model/DAO.class.php');
 
 
 try{
-  $db = new DAO();
+  $db = DAO::get();
 
   $liste = $db->getEmails();
   echo '<pre>' . var_export($liste, true) . '</pre>';
@@ -58,6 +58,14 @@ try{
     echo "</br>False ";
   }
 
+
+  echo "</br>getCoachOuCandidat(adressemail) : adresse-candidat@gmail.com  : (doit retourner type Coach)";
+  $testLogin = $db->getCoachOuCandidat("adresse-candidat@gmail.com","motdepassecandidat");
+  if ($testLogin){
+    echo '<pre>' . var_export($testLogin, true) . '</pre>';
+  }else{
+    echo "</br>False ";
+  }
   
 
 
