d9ebb6eab484d4adb688b9322420bcf2c577b93e
arlea <alexandre.arle@etu.univ-grenoble-alpes.fr>
2021-12-04 14:01:26 +0100

    un peu de css

--- Site/view/design/navigation.php
+++ Site/view/design/navigation.php
@@ -34,12 +34,15 @@ session_start();
           
             <li><?= get_class($_SESSION['utilisateur']) ?></li>
 
-            <?php if (is_a($_SESSION['utilisateur'],"Candidat")) : ?>
-              <li><a href="../controler/formulaire.ctrl.php">Mon Recrutement</a></li>
-
-            <?php endif; ?>
+            <div id="menu-part2">
+              <?php if (is_a($_SESSION['utilisateur'],"Candidat")) : ?>
+                <li><a href="#"><i class="fa fa-user"></i>Mon profil</a></li>
+                <li><a href="#"><i class="fa fa-pencil-square-o"></i>Editez mon profil</a></li>
+                <li><a href="../controler/formulaire.ctrl.php">Mon Recrutement</a></li>
+              <?php endif; ?>
+            </div>
             <form action="../controler/main.ctrl.php" method="post">
-              <li><button id="btn-logout" type="submit" name="logout" value="true">Déconnexion</button></li>
+              <li><button id="btn-logout" type="submit" name="logout" value="true"><i class="fa fa-sign-out" aria-hidden="true"></i>Déconnexion</button></li>
 
             </form>
           </ul>
