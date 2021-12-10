0ee53225b3c110ddc7bd815552f89fcd489a7dff
HELLO Adam <helloa@hostname.iut2.upmf-grenoble.fr>
2021-12-10 16:53:08 +0100

    parrainer view et ctrl

---
+++ Site/controler/parrainer.ctrl.php
@@ -0,0 +1,9 @@
+<?php
+
+$nomCandidat = (isset($_POST['nomCandidat'])) ? $_POST['nomCandidat']:"";
+$nomCandidat = (isset($_POST['telCandidat'])) ? $_POST['telCandidat']:"";
+$nomParrainer = (isset($_POST['nomParrainer'])) ? $_POST['nomParrain']:"";
+$telCandidat = (isset($_POST['telParrainer'])) ? $_POST['telParrain']:"";
+
+
+?>
