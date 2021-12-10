f71775dd30ba92112cfec32efa78ab3539862bfa
Remi Del Medico <remi.del-medico@etu.univ-grenoble-alpes.fr>
2021-12-10 13:25:05 +0100

    Update Site/test/Candidat.test.php

--- Site/test/Candidat.test.php
+++ Site/test/Candidat.test.php
@@ -1,13 +1,7 @@
 <?php
 
 try{
-  $dbconn3 = pg_connect("host=localhost port=5432 dbname=testdao user=testdao password=test");//138.68.96.182
-  $req = pg_query($dbconn3,"SELECT * FROM article");
-
-  
-  $f = pg_fetch_all($req);
-
-  var_dump($f);
+  $db = DAO::get();//138.68.96.182
 
 }catch (Exception $e) {
   print("\n*** Erreur ***\n");
@@ -23,14 +17,17 @@ try {
 
   //Test de la récupération d'un candidat
   print("Accès à un candidat : ");
-  $expected = new Candidat('IBM','17 avenue de Europe 92275 Bois-Colombes','0476624800', 'ibm@ibm.com'); // Candidat attendue
-  $value = $dao->getCandidat(1); // On prend le candidat d'id 1
+  $expected = new Candidat('adresse-candidat@gmail.com'); // Candidat attendue
+
+//function __construct(string $mail, string $password,string $nom='', string $prenom='', int $age=0, string $telephone='', string $lienCV='', string $lienLM='') {
+
+  $value = $dao->getCandidat(966); // On prend le candidat d'id 966
   if ( $value != $expected) { //On compare le candidat récuperé et celui attendue
     print("\n");
     var_dump($value);
     print("\nAttendu : \n");
     var_dump($expected);
-    throw new Exception("Lecture du candidat N°1 incorrecte");
+    throw new Exception("Lecture du candidat N°966 incorrecte");
   }
   print("OK\n");
 
