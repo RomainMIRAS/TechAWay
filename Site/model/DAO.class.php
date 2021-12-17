829b17c8541e8b5edc08d50e8f738a76607c60f9
devkosov <devkosov@github.com>
2021-12-17 15:05:10 +0100

    test

--- Site/model/DAO.class.php
+++ Site/model/DAO.class.php
@@ -201,7 +201,7 @@ function getId($adressemail): int{
 		// Affiche en clair l'erreur PDO si la requête ne peut pas s'exécuter
 
 		$table = pg_fetch_all($req);
-
+		var_dump($req);
 		$id = intval($req['idutilisateur']);
 		// Tests d'erreurs
 	if (count($table) == 0) {
