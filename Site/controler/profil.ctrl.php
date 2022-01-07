1c3836fd3891751ffce66253dfec6305dc7e9ad7
ARLE Alexandre <arlea@hostname.iut2.upmf-grenoble.fr>
2022-01-07 14:54:54 +0100

    page profil

--- Site/controler/profil.ctrl.php
+++ Site/controler/profil.ctrl.php
@@ -9,25 +9,20 @@ include_once(__DIR__."/../model/Utilisateur.class.php");
 include_once(__DIR__."/../model/Candidat.class.php");
 include_once(__DIR__."/../model/Coach.class.php");
 
-$view = new View();
-
-if (isset($_SESSION['utilisateur'])) { /* Si la variable session 'utilisateur' existe */
+$view = new View(); 
     
-    echo "test";
-    
-    /* En fonction de l'étape */
-    switch ($_SESSION['utilisateur']->getEtape()) {
-        case 1:
-            $etapeDetail = "Vous venez de definir votre profil, dès lors vous pouvez consulter les offres qui peuvent potentiellement vous intéresser.";
-            break;
+/* En fonction de l'étape */
+switch ($_SESSION['utilisateur']->getEtape()) {
+    case 1:
+        $etapeDetail = "Vous venez de definir votre profil, dès lors vous pouvez consulter les offres qui peuvent potentiellement vous intéresser.";
+        break;
         
-        case 2:
-            $etapeDetail = "";
-            break;
-    }
-    $view->assign("etapeDetail",$etapeDetail);
-
+    case 2:
+        $etapeDetail = "";
+        break;
 }
+$view->assign("etapeDetail",$etapeDetail);
+
 
 
 // Charge la vue
