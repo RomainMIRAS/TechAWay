539962fc8de4db2dd3609ef4a805ff6794e8823b
RRAHMANI Altin <rrahmaal@hostname.iut2.upmf-grenoble.fr>
2022-01-07 15:18:40 +0100

    DAO++

--- Site/model/DAO.class.php
+++ Site/model/DAO.class.php
@@ -72,7 +72,7 @@ function createUtilisateur(string $mail, string $pass) { //returns boolean
 		INSERT INTO candidat values($sel,'','',0,'','',$comp,$rens);
 		";
 
-		$res = pg_query($this->db, $r);
+		$res = @pg_query($this->db, $r);
 
 		if($res){
 			return true;
