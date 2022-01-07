9ab6dee6905fa3288fe77cc3dc865cce406712ff
ARLE Alexandre <arlea@hostname.iut2.upmf-grenoble.fr>
2022-01-07 15:51:34 +0100

    page tableau coach

--- Site/view/tableau.view.php
+++ Site/view/tableau.view.php
@@ -1,6 +1,6 @@
 <?php
-    include_once(__DIR__."/../../model/Utilisateur.class.php");
-    include_once(__DIR__."/../../model/Candidat.class.php");
+    include_once(__DIR__."/../model/Utilisateur.class.php");
+    include_once(__DIR__."/../model/Candidat.class.php");
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
