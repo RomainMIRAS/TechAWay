30989a1d6da2f5a2a10a32007a85ba396475c640
RRAHMANI Altin <rrahmaal@hostname.iut2.upmf-grenoble.fr>
2022-01-07 16:12:38 +0100

    DAO++

--- Site/model/Candidat.class.php
+++ Site/model/Candidat.class.php
@@ -16,15 +16,15 @@ class Candidat extends Utilisateur {
 
   // Contructeur
   // Revoir Constructeur
-function __construct(string $mail, string $password,string $nom='', string $prenom='', int $age=0, string $telephone='', string $lienCV='', string $lienLM='', int $etape = 0, string $pays ='' , string $ville = '', string $dateCreation = ''  /*Competence $competenceAcquis = null, Renseignement $preference = 0 */ ) {
+function __construct(string $mail, string $password,string $nom='', string $prenom='', int $age=0, string $telephone='', string $lienCV='', string $lienLM='', int $etape = 0, string $pays ='' , string $ville = '', string $dateCreation = '' , Competence $competenceAcquis, Renseignement $preference  ) {
     parent::__construct($mail, $password, $nom, $prenom, $age, $telephone, $dateCreation);
     $this->lienCV = $lienCV;
     $this->lienLM = $lienLM;
     $this->etape = $etape;
     $this->pays = $pays;
     $this->ville = $ville;
-    // $this->competenceAcquis = $competenceAcquis;
-    // $this->preference = $competenceAcquis;
+    $this->competenceAcquis = $competenceAcquis;
+    $this->preference = $competenceAcquis;
     // $this->discussions = null;
   }
 
