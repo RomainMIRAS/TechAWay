cb1001bfefa023cd1a18eb195fd6ece88c79ed0f
Remi Del Medico <remi.del-medico@etu.univ-grenoble-alpes.fr>
2022-01-07 15:54:48 +0100

    Update Site/controler/RechercheEntreprise.ctrl.php

--- Site/controler/RechercheEntreprise.ctrl.php
+++ Site/controler/RechercheEntreprise.ctrl.php
@@ -18,7 +18,10 @@ $email = (isset($_POST['email'])) ? $_POST['email']:"";
 $password = (isset($_POST['password'])) ? $_POST['password']:"";
 $checkpassword = (isset($_POST['checkpassword'])) ? $_POST['checkpassword']:"";
 
+
+session_start();
 $competence = $_SESSION['utilisateur']->getCompetenceAcquis();
+session_write_close();
 ///////////////////////////////////////////////////////////////////////////////
 // Partie View
 ///////////////////////////////////////////////////////////////////////////////
