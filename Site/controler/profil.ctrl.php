b722f42184ee39134c9c9ac4fce936bee82bf2fb
arlea <alexandre.arle@etu.univ-grenoble-alpes.fr>
2022-01-08 18:11:00 +0100

    page profil

--- Site/controler/profil.ctrl.php
+++ Site/controler/profil.ctrl.php
@@ -12,13 +12,12 @@ include_once(__DIR__."/../model/Coach.class.php");
 $view = new View();
 
 /* On récupère le lien photo */
-$photoLink = $_POST['link-photo'] ?? '';
 
-if (is_uploaded_file($_FILES[$photoLink]['tmp_name'])) {
-    echo "File ". $_FILES[$photoLink]['name'] ." téléchargé avec succès.\n";
+if (is_uploaded_file($_FILES['link-photo']['tmp_name'])) {
+    echo "File ". $_FILES['link-photo']['name'] ." téléchargé avec succès.\n";
 } else {
     echo "Attaque possible par téléchargement de fichier : ";
-    echo "Nom du fichier : '". $_FILES[$photoLink]['tmp_name'] . "'.";
+    echo "Nom du fichier : '". $_FILES['link-photo']['tmp_name'] . "'.";
 }
 
 // Passage des paramètres
