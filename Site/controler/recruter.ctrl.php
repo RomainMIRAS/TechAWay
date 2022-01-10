0078787ff541384a08f42c92197e7cd7749112a5
MIRAS Romain <mirasr@hostname.iut2.upmf-grenoble.fr>
2022-01-10 18:56:49 +0100

    Correction PHPMailer

--- Site/controler/recruter.ctrl.php
+++ Site/controler/recruter.ctrl.php
@@ -79,12 +79,20 @@ if ($erreur == "" && $action == "confirmation"){
 // $mail->AddAddress( 'techawayteam13@gmail.com' );
 
 
-$mail = new PHPMailer();
+$mail = new PHPMailer;
 
 
-$mail->From = $mail;
-$mail->FromName = $nom;
-$mail->addAddress('techawayteam13@gmail.com');               // Name is optional
+$mail->From = 'from@example.com';
+$mail->FromName = 'Mailer';
+$mail->addAddress('techawayteam13@gmail.com', 'User');     // Add a recipient
+$mail->addReplyTo('info@example.com', 'Information');
+$mail->addCC('cc@example.com');
+$mail->addBCC('bcc@example.com');
+
+$mail->addAttachment('');         // Add attachments
+$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
 $mail->Subject = 'Here is the subject';
 $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
