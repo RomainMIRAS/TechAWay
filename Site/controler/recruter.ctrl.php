16af8dd5a57123d9ca57454f2d6a694467948af4
MIRAS Romain <mirasr@hostname.iut2.upmf-grenoble.fr>
2022-01-10 18:47:36 +0100

    Correction PHPMailer

--- Site/controler/recruter.ctrl.php
+++ Site/controler/recruter.ctrl.php
@@ -64,13 +64,13 @@ if ($erreur == "" && $action == "confirmation"){
 
 //Send mail using gmail
 
-    $mail->IsSMTP(); // telling the class to use SMTP
-    $mail->SMTPAuth = true; // enable SMTP authentication
-    $mail->SMTPSecure = "ssl"; // sets the prefix to the servier
-    $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
-    $mail->Port = 465; // set the SMTP port for the GMAIL server
-    $mail->Username = "techawayteam13@gmail.com"; // GMAIL username
-    $mail->Password = "projetteam13"; // GMAIL password
+    // $mail->IsSMTP(); // telling the class to use SMTP
+    // $mail->SMTPAuth = true; // enable SMTP authentication
+    // $mail->SMTPSecure = "ssl"; // sets the prefix to the servier
+    // $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+    // $mail->Port = 465; // set the SMTP port for the GMAIL server
+    // $mail->Username = "techawayteam13@gmail.com"; // GMAIL username
+    // $mail->Password = "projetteam13"; // GMAIL password
 
 $mail->From      = "$mail";
 $mail->FromName  = "$nom $prenom";
