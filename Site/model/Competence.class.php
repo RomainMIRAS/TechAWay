3f33967e915e9e36af7c8136208e126dd98bbbb5
MIRAS Romain <mirasr@hostname.iut2.upmf-grenoble.fr>
2022-01-10 20:44:16 +0100

    Correction Compétence

--- Site/model/Competence.class.php
+++ Site/model/Competence.class.php
@@ -12,8 +12,8 @@ class Competence {
   function __construct(int $id,string $nvEtude = '', string $langeParle = "", string $langageAcquis = "") {
     $this->id = $id;
     $this->nvEtude = $nvEtude;
-    $this->langeParle = conversionStringArray($langeParle);
-    $this->langageAcquis =conversionStringArray($langageAcquis);
+    $this->langeParle = this->conversionStringArray($langeParle);
+    $this->langageAcquis = this->conversionStringArray($langageAcquis);
   }
 
   function conversionStringArray(string $chaine){
