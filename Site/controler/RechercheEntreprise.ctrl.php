76ae697e8b8ea2a96896481dbb15c1df7e35d6ca
Remi Del Medico <remi.del-medico@etu.univ-grenoble-alpes.fr>
2022-01-10 12:57:37 +0100

    Update Site/controler/RechercheEntreprise.ctrl.php, Site/controler/tableau.ctrl.php

--- Site/controler/RechercheEntreprise.ctrl.php
+++ Site/controler/RechercheEntreprise.ctrl.php
@@ -13,22 +13,33 @@ include_once(__DIR__."/../model/Renseignement.class.php");
 include_once(__DIR__."/../model/Offre.class.php");
 include_once(__DIR__."/../model/Candidat.class.php");
 
+$db = DAO::get(); // on récupère l'unique instance 
 
 
 session_start();
 $candidat = $_SESSION['utilisateur'];
 session_write_close();
-//$competence = $candidat->getCompetenceAcquis();
 
-//$nvEtude = $competence->getnvEtude();
+
+$offres = $db->getOffres();
+$nbOffres = $db->nombreOffres();
+
+
+foreach($offres as $o):
+    $compet = $o->getCompetenceRecherche();
+    $rensei = $o->getDetailOffre();
+    //$compet->getNvEtude(); ==> attente liste déroulante pour chaque niveau étude
+    //$compet->getLangeParle(); ==> attente liste déroulante à choix multiple (https://support.gainsight.com/SFDC_Edition/Data_Management/Managing_Data_In_Gainsight/Dropdown_List_and_Multi_Select_Dropdown_List)
+    //$compet->getLangageAcquis(); ==> attente liste déroulante avec principaux langages
+    
+endforeach;
+
 ///////////////////////////////////////////////////////////////////////////////
 // Partie View
 ///////////////////////////////////////////////////////////////////////////////
 
 $view = new View();
 
-//$view->assign('competence',$competence);
-//$view->assign('etude',$nvEtude);
 $view->assign('candidat',$candidat);
 $view->display("RechercheEntreprise.view.php");
 
