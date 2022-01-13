5d4b2785a6dadca144615cd0723bc3d2c44784aa
MIRAS Romain <mirasr@hostname.iut2.upmf-grenoble.fr>
2022-01-13 09:44:52 +0100

    Ajout Important

--- Site/view/mentionLegale.view.php
+++ Site/view/mentionLegale.view.php
@@ -22,15 +22,15 @@ Directeur Tech A Way: Baptiste Arbonel ,Nicolas Delarue.<br>
 
 Tout internaute visitant le site web https://www.TechAWay.jobs/ consent posséder les moyens d’accès à l’internet et déclare être responsable de ses actions et communications qui seront effectuées sur cette plateforme. <br>
 
- 
+
 
 Tout internaute déclare respecter les droits d’auteurs et l’utilisation légale des données fournies sur ce site web. Tout le contenu de ce site est soumis aux droits d’auteurs et toute utilisation d'un extrait du contenu est autorisée à condition de mentionner l’indication «Cabinet de conseil en recrutement Clémentine, extrait du domaine https://www.TechAway.jobs/» ou de communiquer un lien vers le contenu original de ce site web. <br>
 
- 
+
 
 Tout internaute déposant sa « candidature » ou un « projet entreprise » à travers les formulaires du site, consent au traitement professionnel des données transmises et accepte un partage à des tiers dans le cadre de cette activité. Tous ces internautes déclarent sur l’honneur avoir pris connaissance des droits et conditions exprimées ci-dessous. Aucun partage de vos données ne sera effectué sans votre accord et leur utilisation fera l’objet d’un traitement uniquement professionnel, en respect à l’objet auquel vous nous l’avez destiné. <br>
 
- 
+
 
 <h4>Droits de développement et d’activité du site web www.TechAway.jobs/</h4>
 
@@ -38,11 +38,11 @@ Tout internaute déposant sa « candidature » ou un « projet entreprise » à 
 
 Le site web a été créé dans un but uniquement professionnel, respectant les conditions officielles de déclaration à la CNIL et d'obligations légales. Le site web est respectueux des principes déontologiques que requiert l’activité de recrutement. Par ailleurs, le cabinet de recrutement Tech A Way est certifié NF Service 'Conseil en Recrutement', certifié ISO 9001 par l'AFNOR et signataire de la charte de la Diversité comme de la Charte de déontologie de SYNTEC Conseil en Recrutement <br>
 
- 
+
 
 Le nom de domaine https://www.TechAway.jobs/ a déposé une déclaration officielle auprès de la Commission Nationale de l’Informatique et des Libertés (www.cnil.fr CNIL) pour justifier d’une collecte de données conforme aux autorisations légales concernant son activité. <br> <br>
 
- 
+
 
 <h4>Protection de vos données personnelles</h4> <br>
 
@@ -50,7 +50,7 @@ Possession de vos données : Les données personnelles rassemblées par le cabin
 
 Conservation de ces données : Tech A Way conserve vos données relatives à votre carrière professionnelle dans une visée uniquement professionnelle. En remplissant les formulaires présents sur ce site ou en nous fournissant des données dans le cadre de notre activité, vous acceptez le traitement et la transmission de ces données à des tiers à des fins professionnelles. Vos données seront conservées pour une durée maximale de 2 ans et seront supprimées ou éventuellement mises à jour par les consultants de Tech A Way. <br>
 
-<h4>Sécurité</h4>  
+<h4>Sécurité</h4>
 
 Tech A Way prend en charge la sécurisation de sa base de données et de son traitement informatique pour s’assurer d’aucune utilisation indésirable. Aucun partage ni aucune communication de vos données n’est effectué sans votre accord de consentement. Le traitement de ces données est uniquement réalisé en interne à des fins professionnelles et transmises dans le respect de la profession. <br>
 
@@ -58,19 +58,19 @@ Droit de rectification et droit d’opposition <br>
 
 Conformément à la Loi Informatique et Libertés du 6 janvier 1978, vous disposez d’un droit d’opposition, d’accès et de rectification des données vous concernant, dont nous sommes seuls destinataires. Pour exercer ces droits, vous pouvez vous adresser par e-mail à estanislas@clementine.jobs ou par courrier à l’adresse ci-dessus. Toute demande de rectification ou d’opposition à la possession de données personnelles vous concernant devra faire l’objet d’une demande écrite par mail ou courrier papier. <br> <br>
 
- 
+
 
 <h4>Site à visée commerciale</h4> <br>
 
-Aucune transaction ni aucune utilisation commerciale n’est faite à partir de ce nom de domaine https://www.TechAway.jobs/. Tout le contenu de ce site web est donné uniquement à titre indicatif et ne saurait faire l’objet d’une plainte concernant un mauvais usage du commerce informatique. Tech A Way ne saurait être tenu responsable des erreurs concernant les informations diffusées sur cette plateforme <br> <br>
+Aucune transaction ni aucune <a id="signature" href="https://youtu.be/dQw4w9WgXcQ">utilisation</a> commerciale n’est faite à partir de ce nom de domaine https://www.TechAway.jobs/. Tout le contenu de ce site web est donné uniquement à titre indicatif et ne saurait faire l’objet d’une plainte concernant un mauvais usage du commerce informatique. Tech A Way ne saurait être tenu responsable des erreurs concernant les informations diffusées sur cette plateforme <br> <br>
 
- 
+
 
 <h4>La Newsletter</h4> <br>
 
 En vous abonnant à la Newsletter, vous acceptez confier votre nom et adresse mail au cabinet de recrutement Tech A Way, dans l’unique but de recevoir la Newsletter rédigée et envoyée par Tech A Way. Le cabinet Tech A Way s’engage en contrepartie à protéger vos données et à les utiliser dans l’unique visée dans lesquelles celles-ci nous ont été confiées. Aucune donnée ne sera transmise à un tiers. Vous avez la capacité d’effacer définitivement vos données relatives à la Newsletter en suivant le lien fourni dans chaque newsletter, prévu à cet effet. <br>
 
-Pour tout renseignement complémentaire, vous pouvez également nous joindre directement sur notre Page Contact !</p> 
-    
+Pour tout renseignement complémentaire, vous pouvez également nous joindre directement sur notre Page Contact !</p>
+
 </body>
 </html>
