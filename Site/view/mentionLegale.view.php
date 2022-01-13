c1b542cc0fffc6d9697672472db082e54e09335a
MIRAS Romain <mirasr@hostname.iut2.upmf-grenoble.fr>
2022-01-13 09:45:41 +0100

    Ajout Important

--- Site/view/mentionLegale.view.php
+++ Site/view/mentionLegale.view.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mention Legale</title>
+    <link rel="stylesheet" type="text/css" href="../view/design/style.css">
+
 </head>
 <body>
     <p>
