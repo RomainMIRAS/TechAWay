b5565b77f6b9fabf7c53ea0e66457191c3756810
Alexandre Arle <alexandre.arle@etu.univ-grenoble-alpes.fr>
2022-01-14 13:53:13 +0100

    Update Site/view/profil.view.php

--- Site/view/profil.view.php
+++ Site/view/profil.view.php
@@ -81,7 +81,7 @@
             <div class="list-check"> <!-- Liste des langues -->
               <?php foreach($langues as $l): ?>
                 <?php if (in_array($l,$_SESSION['utilisateur']->getCompetenceAcquis()->getLangeParle())): ?>
-                  <input type="checkbox" name="langueParle[]" value="<?php echo strtolower($l) ?>" selected><?= $l ?></option>
+                  <input type="checkbox" name="langueParle[]" value="<?php echo strtolower($l) ?>" checked><?= $l ?></option>
                 <?php else: ?> 
                   <input type="checkbox" name="langueParle[]" value="<?php echo strtolower($l) ?>"><?= $l ?></option>
                 <?php endif; ?>
