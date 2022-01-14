9e2628788e14128608b208a0e95c1e1b527b47ad
Remi Del Medico <remi.del-medico@etu.univ-grenoble-alpes.fr>
2022-01-14 21:50:57 +0100

    Update Site/controler/offre.ctrl.php

--- Site/controler/offre.ctrl.php
+++ Site/controler/offre.ctrl.php
@@ -49,6 +49,8 @@ $view = new View();
 
 $action = $_POST['action'] ?? '';
 $message = '';
+$candidat->setEtape(2);
+        $db->updateCandidat($candidat);
 if ($action=='supprY') {
         $candidat->setLienLM("");
         
