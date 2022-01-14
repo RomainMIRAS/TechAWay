bf384c2e42eb37cb4098d0f1f5a9adaa21221110
Remi Del Medico <remi.del-medico@etu.univ-grenoble-alpes.fr>
2022-01-14 14:55:05 +0100

    Update Site/view/RechercheEntreprise.view.php

--- Site/view/RechercheEntreprise.view.php
+++ Site/view/RechercheEntreprise.view.php
@@ -48,7 +48,6 @@
                 <th>Mail contact</th>
                 <th>Score de match</th>
                 <th>Postuler</th>
-                <th>Postuler22</th>
             </tr>
             <?php krsort($listeOffreMatch) ?>
             <?php foreach (array_keys($listeOffreMatch) as $key) : ?> <!-- pour chaque candidat -->
@@ -62,9 +61,6 @@
                         <td><?= $listeOffreMatch[$key]->getEntreprise()->getTelephone() ?></td>
                         <td><?= $listeOffreMatch[$key]->getEntreprise()->getMail() ?></td>
                         <td><?= round((($key+473)*100)/(616)) ?>%</td>
-                        <td class="sup">
-                          <button class="editBtn"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-                        </td>
                         <td class="sup">
                           <form action="tableau.ctrl.php" method="POST">
                             <input type="hidden" class="candidatAction" name="candidatAction" value="deleteN">
