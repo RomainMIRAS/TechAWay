e87ab66d815ec6088fd028ca9f086ab818568f3c
arlea <alexandre.arle@etu.univ-grenoble-alpes.fr>
2022-01-16 21:37:29 +0100

    css sur offre

--- Site/view/offre.view.php
+++ Site/view/offre.view.php
@@ -66,8 +66,7 @@
         <!-- On affiche toutes les infos sur l'offre -->
         <h2><?php echo "$nom"; ?></h2>
         <span class="asterisque">Date de création : <?php echo "$date"; ?></span>
-
-        <h3>Statut : <p>en attente d'une réponse de l'entreprise.</p></h3>
+        <span class="asterisque">Statut : en attente d'une réponse de l'entreprise.</span>
 
         <h2>Informations</h2>
             <ol>
